c0ce828bf2b285674c136f2fcf2d883535e56c11
wcy <2067094042@qq.com>
2019-12-02 14:53:00 +0800

    再次对启动类进行修改

--- src/main/java/com/qf/SpringBootUserApplication.java
+++ src/main/java/com/qf/SpringBootUserApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootUserApplication {
     public static void main(String[] args) {
         System.out.println("第一个git");
+        System.out.println("我有修改了");
         SpringApplication.run(SpringBootUserApplication.class);
     }
 }
